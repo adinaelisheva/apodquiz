1f078e47a28ed7bc7e2371bacd696fc4e991881e
Adina Rubinoff <adinarubinoff@gmail.com>
2018-11-16 15:03:33 +0000

    cookie decoding

--- index.js
+++ index.js
@@ -3,12 +3,22 @@
   let completed = 0;
   let numQs;
 
-  const input = 'nopqrstuvwxyz0123456789abcdefghijklm';
-  const output = 'abcdefghijklmnopqrstuvwxyz0123456789';
   const decode = (str) => {
+    const input = 'nopqrstuvwxyz0123456789abcdefghijklm';
+    const output = 'abcdefghijklmnopqrstuvwxyz0123456789';
     var translate = x => input.indexOf(x) > -1 ? output[input.indexOf(x)] : x;
     return str.split('').map(translate).join('');
-  }
+  };
+  
+  const getMiniDateStr = (date) => {
+    let m = date.getMonth();
+    m = m.length < 2 ? `0${m}` : m;
+    let d = date.getDate();
+    d = d.length < 2 ? `0${d}` : d; 
+    return `${m}-${d}-${date.getFullYear()}`;
+  };
+
+  const cookiename = 'lastcompleted';
 
   const completeQuiz = () => {
     document.querySelector('.finished').classList.remove('invisible');
@@ -16,26 +26,40 @@
     date.setTime(date.getTime() + (2*24*60*60*1000)); // Expire in 2 days from now
     const expires = date.toUTCString();
     // value is today's date as a string
-    let m = date.getMonth();
-    m = m.length < 2 ? `0${m}` : m;
-    let d = date.getDate();
-    d = d.length < 2 ? `0${d}` : d; 
-    const completed = `${m}-${d}-${date.getFullYear()}`;
-    document.cookie = `lastcompleted=${completed};expires=${expires};path=/`;
+    const completed = getMiniDateStr(date);
+    document.cookie = `${cookiename}=${completed};expires=${expires};path=/`;
   };
+
+  const hasQuizAlreadyBeenCompleted = () => {
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+      const parts = cookies[i].split('=');
+      if (parts[0] !== cookiename) {  
+        continue;
+      }
+      const lastDate = parts[1];
+      const today = new Date();
+      return lastDate === getMiniDateStr(today);
+    }
+    // If we got here there's no lastcompleted cookie
+    return false;
+  }
+
+  completeQuestion = (input) => {
+    input.classList.add('completed');
+    input.setAttribute('disabled','true');
+    completed++;
+    score.querySelector('span.num').innerHTML = completed;
+    if (completed >= numQs) {
+      completeQuiz();
+    }
+  }
 
   const verifyAnswer = (e) => {
     const input = e.target;
     const answer = input.parentElement.querySelector('.answer');
     if (input.value.toLowerCase() === decode(answer.innerText.toLowerCase())) {
-      const style = input.getAttribute('style');
-      input.classList.add('completed');
-      input.setAttribute('disabled','true');
-      completed++;
-      score.querySelector('span.num').innerHTML = completed;
-    }
-    if (completed >= numQs) {
-      completeQuiz();
+      completeQuestion(input);
     }
   };
 
@@ -46,21 +70,35 @@
   };
 
   window.onload = () => {
+    const quizAlreadyCompleted = hasQuizAlreadyBeenCompleted();
+
     const questions = document.querySelectorAll('input.blank');
     numQs = questions.length;
+    
+    score = document.querySelector('.score');
+    score.querySelector('.whole').innerText = numQs;
+    
     questions.forEach((input) => {
       const answerDiv = input.parentElement.querySelector('.answer');
       const answer = answerDiv.innerText.toLowerCase().trim();
       answerDiv.innerHTML = answer;
+      
       const width = Math.max(40, answer.length*10);
       input.setAttribute('style', `width:${width}px;`);
-      input.onkeyup = (e) => { verifyAnswer(e); };
+
+      if (quizAlreadyCompleted) {
+        input.innerHTML = decode(answer);
+        completeQuestion(input);
+      } else {
+        // No point in setting a listener if the input is already completed
+        input.onkeyup = (e) => { verifyAnswer(e); };
+      }
     });
+
     document.querySelector('.openlinks').onclick = () => {
       openAllLinks();
     };
-    score = document.querySelector('.score');
-    score.querySelector('.whole').innerText = questions.length;
+
     document.querySelector('.quiz').classList.remove('hidden');
   };
 
